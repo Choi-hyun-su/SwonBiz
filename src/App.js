1254ac75d88d951beda1b3323cedc794e67480d0
swonbiz <chs071368@naver.com>
2020-10-10 04:18:31 +0900

    maps create

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers=[
+  {
+    'id':1,
+    'name':'신사임당',
+    'job':'화가',
+    'birth':'901111',
+    'image':'https://placeimg.com/64/64/1',
+    'gender':'여자'
+  },
+  {
+    'id':2,
+    'name':'임깍정',
+    'job':'전업남편',
+    'birth':'931212',
+    'image':'https://placeimg.com/64/64/2',
+    'gender':'남자'
+  },
+  {
+    'id':1,
+    'name':'홍길동',
+    'job':'산적',
+    'birth':'930505',
+    'image':'https://placeimg.com/64/64/3',
+    'gender':'남자'
+  }
+]
 
 class App extends Component {
   render(){
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2> Let's develop   ddddddd Swonbiz System~~~~!!!!</h2>
-      </div>
-    );
+      customers.map(c => {
+        return( <Customer key={c.id} id={c.id} name={c.name} birth={c.birth} job={c.job} image={c.image} gender={c.gender} />);})
+        );
   }
   
 }
